60a0425db5177c4e7de2100d530db47d323feba4
james-mungai <jamesmungai.code@gmail.com>
2020-04-15 22:29:24 +0300

    Updates

---
+++ precache-manifest.3bfdf53f830fefd271425b94a7fca55d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f247194d0067035c0f458290a44dfeda",
+    "url": "/estimator-front/index.html"
+  },
+  {
+    "revision": "c90f132c12e76d497229",
+    "url": "/estimator-front/static/css/main.7f2c7183.chunk.css"
+  },
+  {
+    "revision": "6a0e5e6d1d2591c2f6e9",
+    "url": "/estimator-front/static/js/2.0caa73ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/estimator-front/static/js/2.0caa73ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c90f132c12e76d497229",
+    "url": "/estimator-front/static/js/main.e9a5066e.chunk.js"
+  },
+  {
+    "revision": "5b6b48e2df3f8fa1e4a6",
+    "url": "/estimator-front/static/js/runtime-main.38138b9e.js"
+  }
+]);
